52b07ddf6c377fe7219cfd0d0e7ae9e20e860d37
Jenny Mo <jennymo@Jennys-MBP.hsd1.ca.comcast.net>
2017-06-08 19:07:11 -0700

    Added ServerInfo class to hold information

---
+++ src/codeu/chat/ServerInfo.java
@@ -0,0 +1,9 @@
+public final class ServerInfo {
+  public final Time startTime;
+  public ServerInfo() {
+    this.startTime = Time.now();
+  }
+  public ServerInfo(Time startTime) {
+    this.startTime = startTime;
+  }
+}
